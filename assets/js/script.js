b2a1e95eaf8cd7b4893161455a330189965f769e
Rasha Alsaleh <rasha.k.alsaleh@gmail.com>
2024-12-20 17:44:28 +0300

    removed outer shadow from tab-items, added another video for the game, renamed some files

--- assets/js/script.js
+++ assets/js/script.js
@@ -103,12 +103,10 @@ const nav = document.querySelector('.nav'),
     tab.addEventListener('click', () => {
         // Remove 'active' class from all tabs and contents
         document.querySelectorAll('.tab-item').forEach(t => t.classList.remove('active'));
-        document.querySelectorAll('.tab-item').forEach(t => t.classList.remove('outer-shadow'));
         document.querySelectorAll('.tab-content').forEach(content => content.classList.remove('active'));
 
         // Add 'active' class to the clicked tab
         tab.classList.add('active');
-        tab.classList.add('outer-shadow');
 
         // Show the corresponding content
         const target = tab.getAttribute('data-target');
